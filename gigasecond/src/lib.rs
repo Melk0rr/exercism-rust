e8e2328283e9144cc3491225917a21bf9a81de42
Me1k0r <runner-me1k0r@proton.me>
2025-06-28 21:47:38 +0200

    edit: exercise/gigasecond - completed

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use time::PrimitiveDateTime as DateTime;
 
+const GIGASECOND: i64 = 1_000_000_000;
+
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    start + time::Duration::seconds(GIGASECOND)
 }
